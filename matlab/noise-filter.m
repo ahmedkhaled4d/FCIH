1822515dcb65b6726dab37e64eb2184c2a5902e4
Ahmed Khaled <ahmed_4d@hotmail.com>
2016-11-02 00:39:44 +0200

    Update noise-filter.m
    random distination

--- matlab/noise-filter.m
+++ matlab/noise-filter.m
@@ -2,9 +2,9 @@ function  h =  makenoise()
     pic = imread('friends.jpg');
     pic = rgb2gray(pic);  % convert it to gray level 
     [height, width] = size(pic); % return vector []
-    dis = round(rand(1)*100)
-    for r = 12:16:height
-        for c = 9:5:width
+    dis = round(rand(1)*10)
+    for r = 12:dis:height
+        for c = 9:dis:width
                 noise = 0 % default 
                 if(round(rand(1)) == 0)
                     noise = 255
