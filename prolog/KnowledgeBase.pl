5f2798e58ee39ef0ec546af8789d2c6ead7d7398
Ahmed Khaled <ahmed_4d@hotmail.com>
2016-11-29 01:18:53 +0200

    Create KnowledgeBase.pl
    FamilyTreeKnowledgeBase
    knowledge base

---
+++ prolog/KnowledgeBase.pl
@@ -0,0 +1,47 @@
+%male( <name> ).
+male( ahmed ).
+male( ali ).
+male( abobakr ).
+male( yasser ).
+male( abdalla ).
+male( hisham ).
+male( samy ).
+
+%female( <name> ).
+female( nada ).
+female( walaa ).
+female( sara ).
+female( hend ).
+female( nora ).
+female( yasmin ).
+female( aya ).
+
+%lifespan( <person's name >, <birth year>, <death year> ).
+lifespan( ahmed, 1968, na ).
+lifespan( ali, 1908, 1980 ).
+lifespan( abobakr, 1890, na ).
+lifespan( yasser, 1864, 1960 ).
+lifespan( abdalla, 1820, 1912 ).
+lifespan( hisham, 1767, 1860 ).
+lifespan( samy, 1846, 1926 ).
+lifespan( nada, 1907, 1980 ).
+lifespan( walaa, 1902, 2006 ).
+lifespan( sara, 1865, 1970 ).
+lifespan( hend, 1822, 1912 ).
+lifespan( nora, 1822, 1914 ).
+lifespan( yasmin, 1768, 1862 ).
+lifespan( aya, 1847, 1928 ).
+
+%parentOf( <parent's name>, <child's name> ).
+parentOf( samy, abobakr ).
+parentOf( aya, abobakr ).
+parentOf( hisham, samy ).
+parentOf( yasmin, samy ).
+parentOf( hisham, abdalla ).
+parentOf( yasmin, abdalla ).
+parentOf( abdalla, yasser ).
+parentOf( tanta_hornblower, yasser ).
+parentOf( yasser, ali ).
+parentOf( sara, ali ).
+parentOf( ali, ahmed ).
+parentOf( nada, ahmed ).
