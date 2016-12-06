c130f1e8117efe06f43014fe7a499b59645583ac
Ahmed Khaled <ahmed_4d@hotmail.com>
2016-12-06 12:50:21 +0200

    add CFs

--- prolog/KnowledgeBase.pl
+++ prolog/KnowledgeBase.pl
@@ -1,104 +1,138 @@
-% ahmed  
+%==========
+%  CFs 
+%==========
+poverty(50).
+illiteracy(50).
+knowledge(90).
+awarness(30).
+ethics(80) .
 no_space(50).
+no_traineer(100).
 money_shortage(50).
 lack_playground(100).
-no_traineer.
-
-
 war(100).
 abuse(100).
-injunes(50).
-genetics(50).
-biological(30).
-enviromental(90).
-
+injuries(50).
+genatics(50).
 
 fact(Prompt) :-
 	write(Prompt),
 	read(X),
 	X == true.
 
-problem(X) :-
-	sport,
-	X = 'sport problem'.
+ethics_problem :-
+	fact('Are the students has ethics issue ?(true/false) ').
 
-% ===========
-% sport field
-% ===========
+poverty_problem :-
+	fact('Are the students suffer from the poverty problem ?(true/false) ').
 
-sport :-
-	not(no_traineer),
-	lack_playground(100).
+illiteracy_problem :-
+	fact('Are the students suffer from illiteracy ?(true/false)') .
 
-lack_playground(CF) :-
-	no_space(CF2),
-	money_shortage(CF3),
-	CF is CF2 + (100-CF2) * CF3/100.
+knowledge_problem :-
+	fact('Are the students suffer from the lack of knowledge ?(true/false)  ').
+
+awarness_problem :-
+	fact('Are the students suffer from the lack of Awarness ?(true/false) ').
 
 
+space_problem :-
+	fact('is the school  suffer from space problem ?(true/false)  ').
 
-%aboubakr
+money_problem :-
+	fact('is the school suffer from the shotage of money ?(true/false)  ').
 
- poverty(50).
- illiteracy(50).
- knowledge(90).
- awarness(30).
- ethics(80) . 
- 
- fact(Prompt) :-
- 	write(Prompt),
- 	read(X),
- 	X == true.
+traineer_problem :-
+	fact('is the school suffer from the shotage of traineers ?(true/false)  ').
 
- ethics_problem :-
- 	fact('is the student has ethics issue ?(true/false) ').
+war_problem :-
+	fact('is the country suffer from the wars ?(true/false)  ').
 
- poverty_problem :-
- 	fact('is the student suffer from the poverty problem ?(true/false) ').
+abuse_problem :-
+	fact('Are the students suffer from the abuse ?(true/false)  ').
 
- illiteracy_problem :-
- 	fact('is the student suffer from illiteracy ?(true/false)') .	
-	
- knowledge_problem :-
- 	fact('is the student suffer from the lack of knowledge ?(true/false)  ').
-	
- awarness_problem :-
- 	fact('is the student suffer from the lack of Awarness ?(true/false) ').
+genatics_problem :-
+	fact('Are the students have Genetic problems ?(true/false)  ').
 
- search(X) :-
- 	school_problem(X).
+injuries_problem :-
+	fact('Are the students suffer from the injuries ?(true/false)  ').
 
- parents_problem(CF):-
- 	poverty(CF1) ,
- 	illiteracy(CF2) ,
- 	CF is (CF1+CF2)/2 . 	
+
+
+
+search(X) :-
+	school_problem(X).
+
+parents_problem(CF):-
+	poverty(CF1) ,
+	illiteracy(CF2) ,
+	CF is (CF1+CF2)/2 .
+
+playground_problem(CF):-
+	no_space(CF1) ,
+    money_shortage(CF2) ,
+	CF is (CF1+CF2)/2 .
+
+enviomental_problem(CF):-
+	abuse(CF1) ,
+    war(CF2) ,
+	CF is (CF1+CF2)/2 .
+
+biological_problem(CF):-
+	genatics(CF1) ,
+    injuries(CF2) ,
+	CF is (CF1+CF2)/2 .
+
 
  school_problem(social) :-
- 	social,
- 	write('it is a social problem with CF = ') ,
- 	ethics(CF1) , 
- 	poverty(CF2) , 
- 	illiteracy(CF3), 
- 	AV is (CF1+CF2+CF3)/3 ,
- 	write(AV) .  
+	social,
+	write('it is a social problem with CF = ') ,
+	ethics(CF1) ,
+	poverty(CF2) ,
+	illiteracy(CF3),
+	AV is (CF1+CF2+CF3)/3 ,
+	write(AV) .
 
  school_problem(cultural) :-
- 	cultural,
- 	awarness(CF),
- 	knowledge(CF1) ,  
- 	poverty(CF2) , 
- 	illiteracy(CF3), 
- 	AV is (CF+CF1+CF2+CF3)/4 ,
- 	write(AV) .  	
+	cultural,
+	awarness(CF),
+	knowledge(CF1) ,
+	parents_problem(CF2) ,
+	AV is (CF+CF1+CF2)/3 ,
+	write(AV) .
+
+school_problem(sport) :-
+	sport,
+	no_traineer(CF),
+	playground_problem(CF1) ,
+	AV is (CF+CF1)/2 ,
+	write(AV) .
+
+school_problem(psychological) :-
+	psychological,
+	enviomental_problem(CF),
+	biological_problem(CF1) ,
+	AV is (CF+CF1)/2 ,
+	write(AV) .
 
  school_problem(X) :-
- 	((social, X = 'social');(cultural, X = 'cultural')).
+	((social, X = 'social');(cultural, X = 'cultural');(sport, X = 'sport');(psychological, X = 'psychological')).
 
- social :-
- 	ethics_problem ,
- 	poverty_problem ,
- 	illiteracy_problem .
-
- cultural :-
- 	(poverty_problem;illiteracy_problem) ,(knowledge_problem ; awarness_problem ).
+sport :-
+	traineer_problem ,
+	(money_problem;space_problem).
  
+social :-
+	ethics_problem ,
+	poverty_problem ,
+	illiteracy_problem .
+
+
+
+cultural :-
+	(poverty_problem;illiteracy_problem) ,(knowledge_problem ; awarness_problem ).
+
+psychological :-
+	(war_problem;abuse_problem) ,
+	(genatics_problem;injuries_problem).
+  
